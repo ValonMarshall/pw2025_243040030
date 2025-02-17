b67b937b1cc57d69d63da54789101f6855fb6681
ValonMarshall <dionavalon32238@gmail.com>
2025-02-17 16:28:50 +0700

    first commit

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,12 @@
+<?php
+    $nama = "Dion Marshall";
+    $email = "dionavalon32238@gmail.com";
+    $pekerjaan = "Mahasiswa";
+    $jurusan = "Teknik Informatika";
+    $universitas = "Universitas Pasundan";
+
+
+    echo "<h1>$nama</h1>";
+    echo "<h4>$email/h4>";
+    echo "<p>Halo, nama saya $nama, saya adalah seorang $pekerjaan $jurusan di $universitas</p>";
+?>
