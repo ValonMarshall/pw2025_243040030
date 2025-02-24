2c9e589ea762b13361fcbaa9d2c6826506bb2312
ValonMarshall <dionavalon32238@gmail.com>
2025-02-24 14:27:44 +0700

    Selesai TUGAS 2

---
+++ tugas/tugas2/2c.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2c.php</title>
+
+    <style>
+        .container {
+            display: flex;
+            flex-direction: column;
+        }
+
+        .kotak {
+            width: 30px;
+            height: 30px;
+            background-color: salmon;
+            border: 0.4px solid black;
+            text-align: center;
+            align-content: center;
+        }
+
+        .row {
+            display: flex;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="container">
+        <?php
+        for ($i = 10; $i >= 1; $i--) {
+            echo '<div class="row">';
+            for ($j = 1; $j <= $i; $j++) {
+                echo "<div class='kotak'>$j</div>";
+            }
+            echo '</div>';
+        }
+        ?>
+    </div>
+
+</body>
+</html>
---
+++ tugas/tugas2/2d.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2d.php</title>
+    
+    <style>
+        .container {
+            display: flex;
+            flex-direction: column;
+        }
+
+        .row {
+            display: flex;
+        }
+
+        .kotak {
+            width: 60px;
+            height: 60px;
+            background-color: salmon;
+            border: 1px solid black;
+            text-align: center;
+            align-content: center;
+        }
+
+        .hitam {
+            background-color: #343434;
+            color: white;
+        }
+
+        .putih {
+            background-color: white;
+            color: #343434;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="container">
+        <?php
+        for ($i = 0; $i < 5; $i++) {
+            echo '<div class="row">';
+            for ($j = 0; $j < 5; $j++) {
+                $class = ($i + $j) % 2 == 0 ? 'hitam' : 'putih';
+                echo "<div class='kotak $class'></div>";
+            }
+            echo '</div>';
+        }
+        ?>
+    </div>
+
+</body>
+</html>
